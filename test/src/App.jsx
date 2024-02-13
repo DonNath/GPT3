63210935a8122faea0c210be1f61e37560bec024
Nathaniel <nathanielayodele179@gmail.com>
2024-02-13 16:41:44 +0100

    linking Each repo and files to the App.js file (The mother Js file)

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
-// import Article from './components/article/article';
-// import Brand from './components/brand/brand';
-// import CTA from './components/cta/cta';
-// import Feature from './components/feature/feature';
-// import Navbar from './components/navbar/navbar';
-
-// all the above can be written as;
-//import {Article, Brand, CTA, Feature, Navbar} from './components
-
-
+import{Blog, Features, Footer, Header, Possibiity, Possibility, WhatGPT3} from './containers';
+import { Article, Brand, CTA, Feature, Navbar } from './components'; //NOTE FEATURE WAS NOT IMPORTED
 
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className = "App">
+      <div className='gradient__bg'>
+       <Navbar/>
+       <Header/>
+      </div>
+        <Brand/>
+        <WhatGPT3/>
+        <Features/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
+
     </div>
   )
 }
