3411aa8b09f851d93aa64a60a2ca1545137a6803
Nathaniel <nathanielayodele179@gmail.com>
2024-02-13 14:26:46 +0100

    initialised all component

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+// import Article from './components/article/article';
+// import Brand from './components/brand/brand';
+// import CTA from './components/cta/cta';
+// import Feature from './components/feature/feature';
+// import Navbar from './components/navbar/navbar';
+
+// all the above can be written as;
+//import {Article, Brand, CTA, Feature, Navbar} from './components
+
+
+
 
 const App = () => {
   return (
